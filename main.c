b7790b95acacd6a1f0c07c3db1afd0820e3c932d
alexgc04 <Alejandro.Gonzalez47@alu.uclm.es>
2023-11-24 10:28:11 +0100

    Add files via upload

---
+++ main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "library.h"
+
+typedef struct empleado{
+    char nombre[100];
+    char apellidos[100];
+    char dni[9];
+};
+
+void main(){
+    int op;
+
+    switch(op) {
+        case 1:
+        case 2:
+        case 3:
+        case 4:
+        case 5:
+        case 6:
+    }
+
+}
